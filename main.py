ba54c4ccb4039af0f8bf37a71462d07070b054a0
sc027008 <325154137+sc027008@users.noreply.github.com>
2026-09-06 12:17:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def generate_chart_url(hourly_rain_list, current_rain_val=0.0):
     step_y2 = get_nice_step(max(max_cum * 1.15, 10.0), steps)
     y2_max = step_y2 * steps
 
-    title_text = "↓棒グラフ: 時間雨量 [mm/h]" + " " * 8 + "折れ線グラフ: 積算雨量 [mm]↓"
+    title_text = "↓棒グラフ: 時間雨量[mm/h]" + " " * 8 + "折れ線グラフ: 積算雨量[mm]↓"
 
     # キャッシュ事故を防ぐユニークファイル名 (ミリ秒タイムスタンプ + ランダム文字列)
     jst = timezone(timedelta(hours=9))
