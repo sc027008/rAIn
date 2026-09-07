b645c5a0ad31be6173221e9a1f2af05c9fd486d1
sc027008 <325154137+sc027008@users.noreply.github.com>
2026-09-08 07:07:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -644,7 +644,7 @@ def main():
         save_state(rain_val, rain_val, last_notified_rank, last_notified_type, last_evening_alert_date)
 
     # 条件4: 夕方（17時0分〜10分）の「今宵アメデス」積算雨量警告通知
-    if now.hour == 17 and (0 <= now.minute <= 10) and not sent_amedes_in_this_run and last_evening_alert_date != today_str:
+    if now.hour == 8 and (0 <= now.minute <= 10) and not sent_amedes_in_this_run and last_evening_alert_date != today_str:
         _, cum_15h, _, chart_url, _ = get_future_cumulative_rain_data(lat, lon, rain_val, ZOOM_LEVEL)
         if cum_15h >= NIGHT_RAIN_THRESHOLD:
             cum_15h_int = int(cum_15h)
