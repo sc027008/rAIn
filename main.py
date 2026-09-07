4fee990347077e0972a1271e5aad20c0385e9b4a
sc027008 <325154137+sc027008@users.noreply.github.com>
2026-09-08 08:40:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -644,14 +644,14 @@ def main():
         save_state(rain_val, rain_val, last_notified_rank, last_notified_type, last_evening_alert_date)
 
     # 条件4: 夕方（17時0分〜10分）の「今宵アメデス」積算雨量警告通知
-    if now.hour == 8 and (0 <= now.minute <= 10) and not sent_amedes_in_this_run and last_evening_alert_date != today_str:
+    if now.hour == 17 and (0 <= now.minute <= 10) and not sent_amedes_in_this_run and last_evening_alert_date != today_str:
         _, cum_15h, _, chart_url, _ = get_future_cumulative_rain_data(lat, lon, rain_val, ZOOM_LEVEL)
         if cum_15h >= NIGHT_RAIN_THRESHOLD:
             cum_15h_int = int(cum_15h)
             formatted_text = f"17～翌8時の積算雨量 <b>{cum_15h_int} mm</b>"
             # ★ 第7引数を today_str から chart_url に修正
             send_google_chat_card(webhook_url, lat, lon, "今宵アメデス", formatted_text, ICON_NIGHT_RAIN, chart_url)
-            # save_state(rain_val, current_rank, last_notified_rank, last_notified_type, today_str)
+            save_state(rain_val, current_rank, last_notified_rank, last_notified_type, today_str)
 
     print("Execution completed successfully.")
 
